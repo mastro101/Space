46c4519319fe2c808e7239c2a1683c81cdfe3662
Matteo Mastropasqua <teo.mastropasqua@gmail.com>
2018-04-05 12:57:49 +0200

    Background & Collider

---
+++ Assets/Script/BackgroundManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundManager : MonoBehaviour
+{
+
+    public Transform[] BGs;
+    public int nextBGIndex { get; set; }
+
+    void Start()
+    {
+        nextBGIndex = BGs.Length;
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Script/BoundingController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoundingController : MonoBehaviour
+{
+    #region EnemyDestroy
+
+    public bool EnemyDestroyer = false;
+
+
+    private void OnEnemyDestroy(IEnemy enemy)
+    {
+        if (!EnemyDestroyer) return;
+        GameObject.Destroy(enemy.gameObject);
+    }
+
+    #endregion
+
+    #region BulletDestroyer
+
+    public bool BulletDestroyer = false;
+
+
+    private void OnBulletDestroy(IBullet bullet)
+    {
+        if (!BulletDestroyer) return;
+        GameObject.Destroy(bullet.gameObject);
+    }
+
+    #endregion
+
+    private void OnTriggerExit(Collider other)
+    {
+        IEnemy enemy = other.gameObject.GetComponent<IEnemy>();
+        if (enemy != null)
+            OnEnemyDestroy(enemy);
+
+    }
+
+
+}
